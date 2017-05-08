a08b289eb259b5a9079c7edf4d4f36767a502b4f
rabanassif <rnassif@stevens.edu>
2017-05-07 20:32:00 -0400

    Create testStrategy2.h

---
+++ testStrategy2.h
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <stdio.h>
+#include <math.h>
+#include <string>
+#include <sstream>
+//#using <System.dll>      // Common Language Runtime Support (/clr)
+#include <fstream>
+#include <cstdlib>
+#include <vector>
+using namespace std; 
+
+class strategy
+{
+public:
+	explicit strategy();
+	void buyOrShort();
+};
