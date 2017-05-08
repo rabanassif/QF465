f4c666052491ad57d333eb3b4aba8dc42cd40f14
rabanassif <rnassif@stevens.edu>
2017-05-07 20:28:15 -0400

    Create testStrategy.h

---
+++ testStrategy.h
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <stdio.h>
+#include <math.h>
+#include <string>
+#include <sstream>
+#include <fstream>
+#include <cstdlib>
+#include <vector>
+using namespace std;
+
+class strategy
+{
+public:
+	explicit strategy();
+	void buyOrShort();
+};
