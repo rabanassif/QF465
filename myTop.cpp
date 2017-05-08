ec9c32222939ece24966c392687e27fb27ea3b70
rabanassif <rnassif@stevens.edu>
2017-05-07 20:33:14 -0400

    Create myTop.cpp

---
+++ myTop.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <stdio.h>
+#include <math.h>
+#include <string>
+#include <sstream>
+//#using <System.dll>      // Common Language Runtime Support (/clr)
+#include <fstream>
+#include "testStrategy2.h"
+using namespace std;
+
+ int main()
+{
+	strategy run;
+	system("pause");
+	return 0;
+}
